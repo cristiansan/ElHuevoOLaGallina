7a53955dc88bea27e3d5dca80d9e9afc53882dc4
horacio_dev <horacio_dev@pmovil.com.ar>
2015-03-16 17:32:08 -0300

    Dalily commit

--- app/src/main/java/app/eggorchicken/eggorchicken/WonActivity.java
+++ app/src/main/java/app/eggorchicken/eggorchicken/WonActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -61,7 +60,7 @@ public class WonActivity extends Activity {
                 params.putString("description", "Tienes 20 segundos para responder la mayor cantidad de afirmaciones reconociendo cuales son verdaderas o falsas! Encuentra una variedad temas muy diferentes para poner a prueba tus conocimientos.\n" +
                         "Juega ahora y pon aprueba tus conocimientos!.");
                 params.putString("link", "https://play.google.com/store/apps/details?id=app.eggorchicken.eggorchicken");
-//                params.putString("picture", "https://raw.github.com/fbsamples/ios-3.x-howtos/master/Images/iossdk_logo.png");
+                params.putString("picture", "https://lh3.ggpht.com/xvYbt8ueE4Ajk4TbB9NADWll6UaM7qYEp8p4vtGexihwxMhqUq7kh3GfLNq-L3bjTSc=w300");
 
                 WebDialog feedDialog = (new WebDialog.FeedDialogBuilder(WonActivity.this, Session.getActiveSession(), params))
                         .setOnCompleteListener(new WebDialog.OnCompleteListener() {
